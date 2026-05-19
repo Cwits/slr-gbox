806abce020c34d7273608ac182e19882d610b59c
Cwits <portablejoe@LAPTOP-CE6JCDFH.localdomain>
2026-05-19 20:31:22 +0200

    Route Manager update fail fix

--- src/ui/display/RouteManager.cpp
+++ src/ui/display/RouteManager.cpp
@@ -139,6 +139,8 @@ RouteManager::~RouteManager() {
 }
 
 void RouteManager::update() {
+    if(!active()) return;
+    
     std::string text = "Routings for ";
     slr::AudioUnitView * view = slr::ProjectView::getProjectView().getUnitById(_currentUnitId);
     text.append(view->name());
